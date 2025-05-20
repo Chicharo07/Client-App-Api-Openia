82c3dba3cbc017a6e42bb5f5d8f9121766467d0d
Chicharo07 <155588449+Chicharo07@users.noreply.github.com>
2025-05-20 09:49:23 -0600

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -7,7 +7,7 @@ async function enviarMensaje() {
         return;
     }
 
-    const url = "http://3.91.173.32/api-gpt-php/endpoints/chat.php";  // Reemplaza con la URL real de tu API
+    const url = "http://44.197.188.160/api-gpt-php/endpoints/chat.php";  // Reemplaza con la URL real de tu API
     const datos = { message: inputMensaje };
 
     try {
